241d60a635d460924499ad3c9e99c2bcb4e0422f
Thang Dinh <vietthang207@gmail.com>
2020-04-04 21:44:52 +0700

    CI=false

--- .travis/build_swap.sh
+++ .travis/build_swap.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 
 printf 'Building swap from commit %s' "$TRAVIS_COMMIT"
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
-  npm run build_auto_dev
-  npm run build_auto_ropsten
+  CI=false npm run build_auto_dev
 else
     echo "Branch is not set for auto-build."
     exit 0
